08448e1890a53608d711b260aeeba64112949ddd
xxo <xxoxec@gmail.com>
2024-06-22 19:41:05 +0900

    Test: 회원 service 로직

---
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,44 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemorymemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemorymemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원 X
+        // Optional<Member> result =
+        // comment+option+m 메스드 뽑는 단축키, ctrl+t
+        validateDuplicateMember(member); // 중복 회원 검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
