723bd0f47d01ae8e0fee341acbda6970c9860031
xxo <xxoxec@gmail.com>
2024-06-24 22:46:06 +0900

    Chore: 기타 변경

--- src/main/java/hello/hello_spring/controller/MemberForm.java
+++ src/main/java/hello/hello_spring/controller/MemberForm.java
@@ -1,5 +1,11 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.domain.Member;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 // 웹 등록 화면에서 데이터를 전달 받을 폼 객체
 public class MemberForm {
 
@@ -11,5 +17,6 @@ public class MemberForm {
 
     public void setName(String name) {
         this.name = name;
+
     }
 }
