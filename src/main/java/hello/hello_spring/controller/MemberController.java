96e0f59b8705c14b6f8cfeea818a4daea57fab7f
xxo <xxoxec@gmail.com>
2024-06-24 20:59:40 +0900

    Comment: 스프링 빈 등록시 싱글톤 등록

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -17,4 +17,10 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    /*
+    스프링은 스프링 컨테이너에 스프링 빈을 등록할 떄, 기본으로 싱클톤으로 등록(유일하게 하나만 등록해서 공유)
+    따라서 같은 스프링 빈이면 모두 같은 인스턴스
+    설정으로 싱글톤이 아니게 설정할 수 있지만, 특별한 경우를 제외하면 대부분 싱글톤
+     */
 }
