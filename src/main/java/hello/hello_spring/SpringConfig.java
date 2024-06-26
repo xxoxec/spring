8ea2c64fb5987701419a2ffeb77dbb3bd3e98af3
xxo <xxoxec@gmail.com>
2024-06-26 23:33:36 +0900

    Feat: spring data jpa 회원 repository를 사용하도록 설정 변경

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -12,31 +12,33 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
     /*
-    JPA는 기존의 반복 코드는 물론이고, 기본적인 SQL도 JPA가 직접 만들어서 실행
-    JPA를 사용하면, SQL과 데이터 중심의 설계에서 객체 중심의 설계로 패러다임을 전환 할 수 있다
-    JPA를 사용하면 개발 생산성을 크게 높일 수 있다
+    스프링 데이터 JPA가 `SpringDataJpaMemberRepository` 를 스프링 빈으로 자동 등록
+    인터페이스를 통한 기본적인 CRUD
+    findByName()` , `findByEmail()` 처럼 메서드 이름 만으로 조회 기능 제공
+    페이징 기능 자동 제공
      */
-    private EntityManager em;
+
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemorymemberRepository();
 //        return new JdbcMemberRepository(dataSource);
         // JdbcTemplate을 사용하도록 스프링 설정 변경
 //        return  new JdbcTemplateMemberRepository(dataSource);
         // JPA를 사용하도록 스프링 설정 변경
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
 
 
