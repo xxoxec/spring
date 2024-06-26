0137b7120cc54f886ef8184c362791fb729b2e9b
xxo <xxoxec@gmail.com>
2024-06-27 00:35:59 +0900

    Feat: 시간 측정 AOP 등록

---
+++ src/main/java/hello/hello_spring/aop/TimeTraceAop.java
@@ -0,0 +1,28 @@
+package hello.hello_spring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/*
+AOP: Aspect Oriented Programming
+공통 관심 사항(cross-cutting concern) vs 핵심 관심 사항(core concern) 분리
+ */
+@Aspect
+@Component
+public class TimeTraceAop {
+
+    @Around("execution(* hello.hello_spring..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString() +" " + timeMs + "ms");
+        }
+    }
+}
