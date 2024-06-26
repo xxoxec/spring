d734ceab4e1a30a531d4c7474a520313b37123ec
xxo <xxoxec@gmail.com>
2024-06-26 22:19:13 +0900

    Feat: jpa service 계층에 트랜잭션 추가

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -5,11 +5,17 @@ import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemorymemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-
+@Transactional
+/*
+스프링은 해당 클래스의 메서드를 실행할 때 트랜잭션을 시작하고, 메서드가 정상 종료되면 트랜잭션을 커밋
+만약 런타임 예외가 발생하면 롤백
+JPA를 통한 모든 데이터 변경은 트랜잭션 안에서 실행해야 한다
+ */
 public class MemberService {
 
     private final MemberRepository memberRepository;
